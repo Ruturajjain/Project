992c70c30ab02d05edc3884c786ae139f8d3831b
Ruturajjain <ruturajjain1998gmail.com>
2022-03-04 12:50:20 +0530

    database changed

--- MoversAndPackers/settings.py
+++ MoversAndPackers/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'MoversAndPackers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'moverpacker',
+        'USER': 'root',
+        'PASSWORD': 'Admin@123',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
